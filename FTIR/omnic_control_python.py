331c204ccbdaaf4b3e37f916e1c867abbb4f5c8e
David Bombara <46980996+dbombara@users.noreply.github.com>
2021-03-14 17:34:21 -0400

    Delete FTIR directory

--- FTIR/omnic_control_python.py
+++
@@ -1,5 +0,0 @@
-# Python Code
-import win32com.client #loads COM library
-OmApp = win32com.client.Dispatch("OmnicApp.OmnicApp")  #connects to Omnic
-OmApp.ExecuteCommand("NewWindow")   #opens a new window in Omnic
-OmApp.Set('Collect NumScans',5)  #sets number of scans to 5
